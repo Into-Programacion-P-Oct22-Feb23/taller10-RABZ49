24b894bdbb1ed0aadd8fa2df5011b9c00fb6be5e
Roberto <rabustamante5@utpl.edu.ec>
2022-12-23 10:57:00 -0500

    Taller 10

--- Taller10/src/paquete2/Problema02.java
+++ Taller10/src/paquete2/Problema02.java
@@ -5,10 +5,39 @@
  */
 package paquete2;
 
+import java.util.Scanner;
+import java.util.Locale;
+
 /**
  *
  * @author reroes
  */
 public class Problema02 {
     
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        entrada.useLocale(Locale.US);
+        
+        int[][] ventasIndividual = new int[2][5];
+        String[] vendedores = {"Jessica Cole", "Robert Wallace"};
+        int suma = 0;
+        
+        for (int i = 0; i < vendedores.length; i++) {
+            System.out.print("-----------------------------------\n");
+            for (int j = 0; j < ventasIndividual[i].length; j++) {
+                System.out.println("Ingrese el número de venta del día: ");
+                ventasIndividual[i][j] = entrada.nextInt();
+                
+                suma = suma + ventasIndividual[i][j];
+                
+            }
+            
+        }
+        for (int i = 0; i < vendedores.length; i++) {
+            System.out.printf("Vendedor(a) %s\n", vendedores[i]);
+            
+        }
+        System.out.printf("Ha realizado un total de %d en ventas.\n", suma);
+        
+    }
 }
